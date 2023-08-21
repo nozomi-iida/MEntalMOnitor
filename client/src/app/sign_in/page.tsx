b8251c78870436ebc54fd65cab440c706f985b95
nozomi-iida <iida19990106@gmail.com>
2023-08-21 16:43:24 +1000

    feat: create next app

---
+++ client/src/app/sign_in/page.tsx
@@ -0,0 +1,10 @@
+import { Logo } from "@/components/logo";
+
+const SignInPage = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+}
+export default SignInPage;
---
+++ client/src/components/logo/index.tsx
@@ -0,0 +1,5 @@
+export const Logo = () => {
+  return (
+    <h1 className="font-logo">MEntal MOnitor</h1>
+  );
+}
