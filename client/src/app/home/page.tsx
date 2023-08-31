7a98e4e7769a8ba845bd57cf1088c9e455ec21de
nozomi-iida <iida19990106@gmail.com>
2023-08-31 15:20:10 +1000

    feat: make it impossible to go home page without sign in

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,6 +1,17 @@
 import { Header } from "@/components/header";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (!session) {
+    redirect("/sign_in");
+  }
+
   return (
     <div>
       <Header />
