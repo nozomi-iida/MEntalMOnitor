b8d2f414c6f95e79aba85ebc262becb205fbc769
nozomi-iida <iida19990106@gmail.com>
2023-09-02 13:47:58 +1000

    feat: sign out

--- client/src/components/text/index.tsx
+++ client/src/components/text/index.tsx
@@ -1,11 +1,34 @@
 import { FC, ReactNode } from "react";
-type Text = {
+type Text = JSX.IntrinsicElements["p"] & {
   children: ReactNode;
   bold?: boolean;
+  color?: "default" | "danger";
 };
 
-export const DefaultText: FC<Text> = ({ children, bold }) => {
+export const DefaultText: FC<Text> = ({
+  children,
+  bold,
+  color = "default",
+  className,
+  ...props
+}) => {
+  const textColor = (() => {
+    switch (color) {
+      case "danger":
+        return "danger";
+      default:
+        return "black";
+    }
+  })();
+
   return (
-    <p className={`${bold ? "font-semibold" : "font-normal"}`}>{children}</p>
+    <p
+      className={`${
+        bold ? "font-semibold" : "font-normal"
+      } text-${textColor} ${className}`}
+      {...props}
+    >
+      {children}
+    </p>
   );
 };
