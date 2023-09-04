e7c191ece705cded7de9bb553fa9760913fdfe1f
nozomi-iida <iida19990106@gmail.com>
2023-09-04 11:26:10 +1000

    fix: change state to ref

--- client/src/app/home/addConditionForm.tsx
+++ client/src/app/home/addConditionForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
+import { useRef } from "react";
 
 export const AddConditionForm = () => {
     const emoji2Num = (emoji: string) => {
@@ -20,14 +20,16 @@ export const AddConditionForm = () => {
         throw new Error("Invalid emoji");
     }
   }
-  const [description, setDescription] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
   const onSubmit = (emoji: string) => {
+    if (!textareaRef.current) {
+      return;
+    }
     const condition = emoji2Num(emoji);
-    console.log(condition, description);
   }
   return (
     <div className="bg-pingBg px-2 pt-4 pb-2 flex flex-col gap-2">
-      <Textarea placeholder="How are you feeling?" onChange={e => setDescription(e.target.value)} />
+      <Textarea ref={textareaRef} placeholder="How are you feeling?" />
       <div className="flex justify-between">
         <Button onClick={() => onSubmit("😆")} variant="ghost" className="text-lg">😆</Button>
         <Button onClick={() => onSubmit("😁")} variant="ghost" className="text-lg">😁</Button>
