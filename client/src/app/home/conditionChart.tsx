ec44cbf5ea5b3b2d39602dc55430c1817b3fd48e
nozomi-iida <iida19990106@gmail.com>
2023-09-06 17:48:12 +1000

    feat: change range with select

--- client/src/app/home/conditionChart.tsx
+++ client/src/app/home/conditionChart.tsx
@@ -8,6 +8,13 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import dayjs from "dayjs";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/ui/select";
+import { useState } from "react";
 
 type Condition = {
   point: number;
@@ -19,38 +26,67 @@ const data: Condition[] = Array.from({ length: 10 }, (_, i) => ({
   createdAt: dayjs().subtract(i, "day").format("YYYY-MM-DD"),
 }));
 
-const xTickFormatter = (date: string) => {
-  return dayjs(date).format("D(dd)");
-};
-
-const formatter = (value: number) => {
-  let emoji;
-  switch (value) {
-    case 100:
-      emoji = "😆";
-      break;
-    case 75:
-      emoji = "😁";
-      break;
-    case 50:
-      emoji = "😐";
-      break;
-    case 25:
-      emoji = "🥲";
-      break;
-    case 0:
-      emoji = "😭";
-      break;
-    default:
-      emoji = "";
-  }
-  return emoji;
-};
+enum Period {
+  Day = "day",
+  Week = "week",
+  Month = "month",
+  Year = "year",
+}
 
 export const ConditionChrt = () => {
+  const [period, setPeriod] = useState<Period>(Period.Day);
+  const xTickFormatter = (date: string) => {
+    switch (period) {
+      case Period.Day:
+        return dayjs(date).format("HH:mm");
+      case Period.Week:
+        return dayjs(date).format("DD(dd)");
+      case Period.Month:
+        return dayjs(date).format("MM");
+      case Period.Year:
+        return dayjs(date).format("YYYY");
+    }
+  };
+
+  const formatter = (value: number) => {
+    let emoji;
+    switch (value) {
+      case 100:
+        emoji = "😆";
+        break;
+      case 75:
+        emoji = "😁";
+        break;
+      case 50:
+        emoji = "😐";
+        break;
+      case 25:
+        emoji = "🥲";
+        break;
+      case 0:
+        emoji = "😭";
+        break;
+      default:
+        emoji = "";
+    }
+    return emoji;
+  };
+
   return (
-    <div>
-      <ResponsiveContainer width="100%" height={300}>
+    <div className="flex flex-col gap-2">
+      <div className="flex justify-end">
+        <Select onValueChange={(value: Period) => setPeriod(value)}>
+          <SelectTrigger className="w-auto">{period}</SelectTrigger>
+          <SelectContent>
+            {Object.values(Period).map((value) => (
+              <SelectItem key={value} value={value}>
+                {value}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+      <ResponsiveContainer width="100%" height={200}>
         <LineChart data={data} margin={{ left: -20, right: 20 }}>
           <Line type="monotone" dataKey="point" stroke="#F894C6" />
           <XAxis dataKey="createdAt" tickFormatter={xTickFormatter} />
